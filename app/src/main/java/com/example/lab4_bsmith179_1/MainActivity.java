bf09bf6762f1fa23bbe4b490da1594c2c8a6c0b8
Bsmith179 <bsmith179@student.cscc.edu>
2025-10-10 00:35:46 -0400

    Updated layout that includes second text field for height. Revised MainActivity.java file to use if/else conditional statements to perform correct BMI calculation, using updated variables relevant to Lab 4's requirements. Applied corrected strings.xml text as applicable.

--- app/src/main/java/com/example/lab4_bsmith179_1/MainActivity.java
+++ app/src/main/java/com/example/lab4_bsmith179_1/MainActivity.java
@@ -19,9 +19,10 @@ import java.text.DecimalFormat;
 public class MainActivity extends AppCompatActivity {
 
     // Declare variables for weight calculation
-    double conversionRate = 2.2;
+
+    double bmi;
     double weightEntered;
-    double convertedWeight;
+    double heightEntered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,39 +36,90 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayUseLogoEnabled(true);
 
         // instantiate needed objects from class
-        final EditText weight = (EditText) findViewById(R.id.tvWeight);
-        final RadioButton lbToKilo = (RadioButton) findViewById(R.id.rbStandard);
-        final RadioButton kiloToLb = (RadioButton) findViewById(R.id.rbMetric);
-        final TextView result = (TextView) findViewById(R.id.tvResult);
-        Button convert = (Button) findViewById(R.id.btConvert);
+        final EditText weight = findViewById(R.id.tvWeight);
+        final EditText height = findViewById(R.id.tvHeight);
+        final RadioButton rbStandard = findViewById(R.id.rbStandard);
+        final RadioButton rbMetric = findViewById(R.id.rbMetric);
+        final TextView result = findViewById(R.id.tvResult);
+        Button convert = findViewById(R.id.btConvert);
 
         convert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // Return if input is empty
+                if (weight.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, getString(R.string.error_enter_weight), Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (height.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, getString(R.string.error_enter_height), Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                heightEntered = Double.parseDouble(height.getText().toString());
                 weightEntered = Double.parseDouble(weight.getText().toString());
                 DecimalFormat tenth = new DecimalFormat("#.#");
 
-                if (lbToKilo.isChecked()) {
-                    if (weightEntered <= 500) {
-                        convertedWeight = weightEntered / conversionRate;
-                        result.setText(tenth.format(convertedWeight) + " kilograms");
-                    } else {
-                        Toast.makeText(MainActivity.this, "Pounds must be less than 500", Toast.LENGTH_LONG).show();
-                    }
+                if (weightEntered <= 0 || heightEntered <= 0) {
+                    Toast.makeText(MainActivity.this, getString(R.string.error_invalid_number), Toast.LENGTH_LONG).show();
+                    return;
                 }
-                if (kiloToLb.isChecked()) {
-                    if (weightEntered <= 225) {
-                        convertedWeight = weightEntered * conversionRate;
-                        result.setText(tenth.format(convertedWeight) + " Pounds");
-                    } else
-                        Toast.makeText(MainActivity.this, "Kilograms must be less than 225", Toast.LENGTH_LONG).show();
+
+                if (rbStandard.isChecked()) {
+                    if (weightEntered > 500) {
+                        Toast.makeText(MainActivity.this, getString(R.string.error_pounds_limit), Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    if (heightEntered > 120) {
+                        Toast.makeText(MainActivity.this, getString(R.string.error_inches_limit), Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    double bmi = (703 * weightEntered) / (heightEntered * heightEntered);
+                    String category = getBMICategory(bmi);
+                    result.setText("Your BMI is: " + tenth.format(bmi) + "\nCategory: " + category);
+
+                } else if (rbMetric.isChecked()) {
+                    if (weightEntered > 225) {
+                        Toast.makeText(MainActivity.this, getString(R.string.error_kilos_limit), Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    if (heightEntered > 300) {
+                        Toast.makeText(MainActivity.this, getString(R.string.error_cm_limit), Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    double heightMeters = heightEntered / 100.0;
+                    double bmi = weightEntered / (heightMeters * heightMeters);
+                    String category = getBMICategory(bmi);
+                    result.setText("Your BMI is: " + tenth.format(bmi) + "\nCategory: " + category);
+
+                } else {
+                    Toast.makeText(MainActivity.this, getString(R.string.error_select_unit), Toast.LENGTH_LONG).show();
                 }
             }
         });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public String getBMICategory(double bmi) {
+        if (bmi < 18.5) {
+            return "Underweight";
+        } else if (bmi < 25) {
+            return "Normal weight";
+        } else if (bmi < 30) {
+            return "Overweight";
+        } else {
+            return "Obese";
+        }
     }
 }
